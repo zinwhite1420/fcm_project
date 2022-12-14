a663b43ba39de52208e7579903a09ef103038a32
zinwhite1420 <zinwhite1420@gmail.com>
2022-12-14 14:14:46 +0900

    최초 수정

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-app-bar title="FCM PROJECT" color="green">
+    </v-app-bar>
+    <v-main>
+      <PushList></PushList>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import PushList from "@/components/PushList";
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    PushList
+
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
